1ce0a34bc25e55880b89ab9adc8c55f216cf7db3
Morsal Niyaz <morsalniyaz@gmail.com>
2021-12-22 15:38:44 -0800

    Added Loading Class

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,13 +2,45 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 
-state = {
-  loading: true
-}
+import Loading from "./Loading.js";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 
 class Dashboard extends Component {
+
+  state = {
+  // loading: true
+  loading: false
+  }
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />
+    }
 
     return <main className={dashboardClasses} />;
   }
