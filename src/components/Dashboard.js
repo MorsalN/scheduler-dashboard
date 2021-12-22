8419b15386ee4ddea91c929e456ae2b1aab3cfdd
Morsal Niyaz <morsalniyaz@gmail.com>
2021-12-22 14:48:37 -0800

    Added Loading.js file

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 
+state = {
+  loading: true
+}
+
 class Dashboard extends Component {
   render() {
     const dashboardClasses = classnames("dashboard");
