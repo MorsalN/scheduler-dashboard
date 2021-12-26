b0a29086d9c507d028d3a601ee16edf09207674f
Morsal Niyaz <morsalniyaz@gmail.com>
2021-12-25 16:05:32 -0800

    Dashboard Connected to scheduler-api and Updateing in Realtime

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,27 +4,35 @@ import classnames from "classnames";
 
 import Loading from "./Loading.js";
 import Panel from "./Panel.js";
+import axios from "axios";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+} from "helpers/selectors";
+import { setInterview } from "helpers/reducers";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
@@ -32,53 +40,94 @@ const data = [
 class Dashboard extends Component {
 
   state = {
-  // loading: true
-  loading: false,
-  focused: null
+    loading: true,
+    // loading: false,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
   }
 
-  /*
-  selectPanel must be an arrow function because of how they handle this context. Arrow functions are designed to alter this behaviour in a specific way. The binding is not dynamic; it is is based on where the function is declared.
+  /* 
+  With these two changes, we can check the browser. Clicking on a panel will enlarge it, and if we reload the browser we will see that it loads the initial state from the localStorage.
   */
-  // Would be an Arrow Function
-  // ex. selectPanel = id => {
-    // this.setState({
-    //   focused: id
-    //  });
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
+
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
+  }
 
   // Instance Method
   selectPanel(id) {
     //By adding prevSate it allow you to click on a panel to enlarge and then click again to go back to show all panels
     this.setState(prevState => ({
-     focused: prevState.focused !== null ? null : id
+      focused: prevState.focused !== null ? null : id
     }));
   }
 
-  
+
   render() {
+
 
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused
-     });
+    });
 
     if (this.state.loading) {
       return <Loading />
     }
 
     const panels = data
-    .filter(
-      panel => this.state.focused === null || this.state.focused === panel.id
-    )
-    .map(panel => (
-      <Panel 
-        key={panel.id}
-        // id={panel.id}
-        label={panel.label}
-        value={panel.value}
-        // We have to use this.selectPanel because we are passing a reference to the instance method as a prop.
-        onSelect={event => this.selectPanel(panel.id)}
-      />
-    ));
+      .filter(
+        panel => this.state.focused === null || this.state.focused === panel.id
+      )
+      .map(panel => (
+        <Panel
+          key={panel.id}
+          // id={panel.id}
+          label={panel.label}
+          value={panel.getValue(this.state)}
+          // We have to use this.selectPanel because we are passing a reference to the instance method as a prop.
+          onSelect={event => this.selectPanel(panel.id)}
+        />
+      ));
     console.log("panels", panels);
 
     // return <main className={dashboardClasses} />;
